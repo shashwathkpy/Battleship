b268f2b7f47f0d29926ba0b3ce8921ab50299844
shashwathkpy <shashwathkpy@gmail.com>
2023-10-02 17:42:29 -0700

    back and forth functionality complete

--- src/game.js
+++ src/game.js
@@ -3,54 +3,34 @@ import Gameboard from './Gameboard.js';
 import Ship from './Ship.js';
 import Player from './Player.js';
 
+
+const playerBoard = new Gameboard();
+playerBoard.placeShip([1,1], false, 5);
+playerBoard.placeShip([3,4], false, 4);
+playerBoard.placeShip([6,5], true, 3);
+playerBoard.placeShip([5,1], false, 3);
+playerBoard.placeShip([8,1], true, 2);
+
+const computerBoard = new Gameboard();
+computerBoard.placeShip([0,0], true, 5);
+computerBoard.placeShip([3,4], false, 4);
+computerBoard.placeShip([6,8], true, 3);
+computerBoard.placeShip([6,2], false, 3);
+computerBoard.placeShip([0,5], true, 2);
+
+const computer = new Player(playerBoard);
+const player = new Player(computerBoard);
+
 function battleship()
 {
-    const playerBoard = new Gameboard();
-    playerBoard.placeShip([1,1], false, 5);
-    playerBoard.placeShip([3,4], false, 4);
-    playerBoard.placeShip([6,5], true, 3);
-    playerBoard.placeShip([5,1], false, 3);
-    playerBoard.placeShip([8,1], true, 2);
-
-    const computerBoard = new Gameboard();
-    computerBoard.placeShip([0,0], true, 5);
-    computerBoard.placeShip([3,4], false, 4);
-    computerBoard.placeShip([6,8], true, 3);
-    computerBoard.placeShip([6,2], false, 3);
-    computerBoard.placeShip([0,5], true, 2);
-
     setupBoard(playerBoard);
     setupBoard(computerBoard);
-    const computer = new Player(playerBoard);
-    const player = new Player(computerBoard);
-    
-    console.log(playerBoard.allSunk());
-    while(!playerBoard.allSunk())
-    {
-        const playerTiles = document.querySelectorAll('.playerTile');
-        let hasHit = computer.randomAttack();
-        while(hasHit == 'alreadyHit')
-        {
-            hasHit = computer.randomAttack();
-        }
 
-        playerTiles.forEach(tile => {
-            const idArray = tile.id.split(',');
-            const coord = [idArray[0],idArray[1]];
-            if(coord == computer.attackedCoord.toString())
-            {
-                if(hasHit)
-                {
-                    tile.style.backgroundColor = 'red';
-                    hasHit = false;
-                }
-                else
-                {
-                    tile.style.backgroundColor = 'lightblue';
-                }
-            }
-        });
-    }
+    console.log(playerBoard.allSunk());
+    // while(!playerBoard.allSunk())
+    // {
+    // 
+    // }
 }
 
 battleship();
@@ -74,7 +54,7 @@ function setupBoard(gb)
             tile.classList.add('tile');
             tile.id = `${i},${j},${boardDiv.id}`;
             // tile.textContent = playerBoard.board[i][j];
-            tile.textContent = `${i},${j}`;
+            // tile.textContent = `${i},${j}`;
             if(gb.board[i][j] instanceof Ship)
             {
                 tile.classList.add('ship');
@@ -82,15 +62,53 @@ function setupBoard(gb)
             if(boardDiv.id == '2')
             {
                 tile.classList.add('computerTile');
+
                 tile.onclick = function ()
                 {
-                    if(player.attack([i,j]))
+                    const playerAttack = player.attack([i,j]);
+                    if(playerAttack == 'alreadyHit')
+                    {
+                        return;
+                    }
+                    if(playerAttack)
                     {
                         this.style.backgroundColor = 'red';
                     }
                     else
                     {
                         this.style.backgroundColor = 'lightblue';
+                    }
+                    tile.style.cursor = 'default';
+                    const playerTiles = document.querySelectorAll('.playerTile');
+                    let computerHit = computer.randomAttack();
+                    while(computerHit == 'alreadyHit')
+                    {
+                        computerHit = computer.randomAttack();
+                    }
+                    playerTiles.forEach(tile => {
+                        const idArray = tile.id.split(',');
+                        const coord = [idArray[0],idArray[1]];
+                        if(coord == computer.attackedCoord.toString())
+                        {
+                            if(computerHit)
+                            {
+                                tile.style.backgroundColor = 'red';
+                                computerHit = false;
+                            }
+                            else
+                            {
+                                tile.style.backgroundColor = 'lightblue';
+                            }
+                        }
+                    });
+                    if(playerBoard.allSunk() || computerBoard.allSunk())
+                    {
+                        console.log('Game Over');
+                        const computerTiles = document.querySelectorAll('.computerTile');
+                        computerTiles.forEach(tile => {
+                            tile.onclick = null;
+                            tile.classList.remove('computerTile');
+                        });
                     }
                 };
             }
@@ -104,24 +122,101 @@ function setupBoard(gb)
 }
 
 
-function attack(tileID)
-{
-    const idArray = tileID.split(',');
-    const coord = [idArray[0],idArray[1]];
-    const boardID = [idArray[2]];
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach(tile => {
-        if(tile.id == tileID && boardID == '2')
-        {
-            if(computerBoard.recieveAttack(coord))
-            {
-                tile.style.backgroundColor = 'red';
-            }
-            else
-            {
-                tile.style.backgroundColor = 'lightblue';
-            }
-            tile.onclick = null;
-        }
-    });
-}
+// function playGame()
+// {
+//     const playerBoard = new Gameboard();
+//     playerBoard.placeShip([1,1], false, 5);
+//     playerBoard.placeShip([3,4], false, 4);
+//     playerBoard.placeShip([6,5], true, 3);
+//     playerBoard.placeShip([5,1], false, 3);
+//     playerBoard.placeShip([8,1], true, 2);
+
+//     const computerBoard = new Gameboard();
+//     computerBoard.placeShip([0,0], true, 5);
+//     computerBoard.placeShip([3,4], false, 4);
+//     computerBoard.placeShip([6,8], true, 3);
+//     computerBoard.placeShip([6,2], false, 3);
+//     computerBoard.placeShip([0,5], true, 2);
+
+//     const playerBoardDiv = document.createElement('div');
+//     playerBoardDiv.id = '1';
+//     playerBoardDiv.classList.add('board');
+//     const body = document.querySelector('body');
+//     body.appendChild(playerBoardDiv);
+
+//     const computerBoardDiv = document.createElement('div');
+//     computerBoardDiv.id = '2';
+//     computerBoardDiv.classList.add('board');
+//     body.appendChild(computerBoardDiv);
+
+//     for(let i = 0; i < playerBoard.board.length; i++)
+//     {
+//         for(let j = 0; j < playerBoard.board[i].length; j++)
+//         {
+//             const tile = document.createElement('div');
+//             tile.classList.add('tile');
+//             tile.classList.add('playerTile');
+//             tile.id = `${i},${j},${playerBoardDiv.id}`;
+//             tile.textContent = `${i},${j}`;
+//             if(playerBoard.board[i][j] instanceof Ship)
+//             {
+//                 tile.classList.add('ship');
+//             }
+//             playerBoardDiv.appendChild(tile);
+//         }
+//     }
+
+//     for(let i = 0; i < computerBoard.board.length; i++)
+//     {
+//         for(let j = 0; j < computerBoard.board[i].length; j++)
+//         {
+//             const tile = document.createElement('div');
+//             tile.classList.add('tile');
+//             tile.classList.add('computerTile');
+//             tile.id = `${i},${j},${computerBoardDiv.id}`;
+//             tile.textContent = `${i},${j}`;
+//             if(computerBoard.board[i][j] instanceof Ship)
+//             {
+//                 tile.classList.add('ship');
+//             }
+//             tile.onclick = function ()
+//                 {
+//                     if(player.attack([i,j]))
+//                     {
+//                         this.style.backgroundColor = 'red';
+//                     }
+//                     else
+//                     {
+//                         this.style.backgroundColor = 'lightblue';
+//                     }
+//                     const playerTiles = document.querySelectorAll('.playerTile');
+//                     let hasHit = computer.randomAttack();
+//                     while(hasHit == 'alreadyHit')
+//                     {
+//                         hasHit = computer.randomAttack();
+//                     }
+            
+//                     playerTiles.forEach(tiles => {
+//                         const idArray = tiles.id.split(',');
+//                         const coord = [idArray[0],idArray[1]];
+//                         if(coord == computer.attackedCoord.toString())
+//                         {
+//                             if(hasHit)
+//                             {
+//                                 tiles.style.backgroundColor = 'red';
+//                                 hasHit = false;
+//                             }
+//                             else
+//                             {
+//                                 tiles.style.backgroundColor = 'lightblue';
+//                             }
+//                         }
+//                     });
+//                 };
+//             computerBoardDiv.appendChild(tile);
+//         }
+//     }
+
+// }
+
+// playGame();
