5a9066db8e3219363615c7d6322e40f290359b37
shashwathkpy <shashwathkpy@gmail.com>
2023-10-12 20:36:37 -0700

    basic ui added

--- src/game.js
+++ src/game.js
@@ -307,20 +307,3 @@ function restart()
         boardArea.removeChild(boardArea.lastChild);
     battleShip();
 }
-
-
-// computer attack new strat
-// create a stack in random order of all possible attacks
-// pop from stack to use an attack
-// if hit add adjacent attacks to stack
-
-// attack adjacent till one hits
-// when an adjacent hits, follow the pattern of direction till ship sinks or miss
-
-// if next hit and ship not sunk then store direction, and push three in line(unless its the border), pop those till ship sinks or misses
-// remove wrong-direction adjacents
-// if miss go to the initial hit and go the opposite direction till ship sinks
-
-// pop already used attacks without using them again
-// hit the next random attack
-// repeat this till either player or computer wins
